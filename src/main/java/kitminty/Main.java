a1b988b40771181b39dcadad0ff016d3c848641a
kitminty <electrodkitty@gmail.com>
2026-03-10 10:10:09 -0400

    dont need depth thing

--- src/main/java/kitminty/Main.java
+++ src/main/java/kitminty/Main.java
@@ -53,7 +53,7 @@ public class Main {
         glVertex2f(+0.0f, +0.5f);
         glEnd();
          */
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT);
         glBegin(GL_LINE_STRIP);
         glColor3f(0.3f, 1.0f, 1.0f);
         glVertex2f(0.0f, 0.0f);
