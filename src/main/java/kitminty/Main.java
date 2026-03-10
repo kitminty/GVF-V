b33d53cb7135b996fda122523d3c590afba1e133
kitminty <electrodkitty@gmail.com>
2026-03-10 14:51:57 -0400

    Point that follows mouse, the coordinates for OpenGL is by -1-1 and the screensize is different, so thats pretty weird

--- src/main/java/kitminty/Main.java
+++ src/main/java/kitminty/Main.java
@@ -1,13 +1,16 @@
 package kitminty;
 
 
-import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.BufferUtils;
 import org.lwjgl.opengl.GL;
+
+import java.awt.*;
+import java.nio.DoubleBuffer;
+import java.nio.IntBuffer;
 
 import static java.sql.Types.NULL;
 import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.opengl.GL11.*;
-import static org.lwjgl.opengl.GL11C.glViewport;
 
 //Isaac was here
 public class Main {
@@ -34,17 +37,21 @@ public class Main {
         glfwSwapInterval(1); // How many draws to swap the buffer
         glfwShowWindow(id); // Shows the window
 
+
         while(!glfwWindowShouldClose(id)) {
-            renderPolygon();
+            renderPolygon(id);
 
             glfwSwapBuffers(id);
             glfwPollEvents();
         }
     }
 
-    static void renderPolygon() {
+    static void renderPolygon(long WindowID) {
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         /*
+        (GetCursorPosX(WindowID)-WindowSizeX(WindowID)/2f)/(WindowSizeX(WindowID)/2f) //for getting the x value of mouse pos
+        -(GetCursorPosY(WindowID)-WindowSizeY(WindowID)/2f)/(WindowSizeY(WindowID)/2f) //for getting the y value of mouse pos
+
         glLineWidth(10.0f); //for GL_LINE_STRIP
         glPointSize(10.0f); //for GL_POINTS
 
@@ -74,7 +81,6 @@ public class Main {
             glVertex2f((float)lemx(i+0.01),(float)lemy(i+0.01));
         }
         glEnd();
-        */
 
         glBegin(GL_LINES);
         glColor3f(0.3f, 1.0f, 1.0f);
@@ -83,6 +89,36 @@ public class Main {
             glVertex2f((float)(0.8*Math.cos(i+0.01)),(float)(0.8*Math.sin(i+0.01)));
         }
         glEnd();
+        */
+
+        glPointSize(10.0f);
+        glBegin(GL_POINTS);
+        glColor3f(1.0f, 0.3f, 0.3f);
+        glVertex2f((float) (GetCursorPosX(WindowID)-WindowSizeX(WindowID)/2f)/(WindowSizeX(WindowID)/2f), (float) -(GetCursorPosY(WindowID)-WindowSizeY(WindowID)/2f)/(WindowSizeY(WindowID)/2f));
+        glEnd();
+    }
+
+    public static double GetCursorPosX(long WindowID) {
+         DoubleBuffer posX = BufferUtils.createDoubleBuffer(1);
+         glfwGetCursorPos(WindowID, posX, null);
+         return posX.get();
+    }
+    public static double GetCursorPosY(long WindowID) {
+        DoubleBuffer posY = BufferUtils.createDoubleBuffer(1);
+        glfwGetCursorPos(WindowID, null, posY);
+        return posY.get();
+    }
+
+    public static int WindowSizeX(long WindowID) {
+        IntBuffer posX = BufferUtils.createIntBuffer(1);
+        glfwGetWindowSize(WindowID, posX, null);
+        return posX.get();
+    }
+
+    public static int WindowSizeY(long WindowID) {
+        IntBuffer posY = BufferUtils.createIntBuffer(1);
+        glfwGetWindowSize(WindowID, null, posY);
+        return posY.get();
     }
 
     public static double lemx(double t) {
