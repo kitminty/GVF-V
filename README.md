a752a88d10701ca1242b8031fa91c0b3e9f28a63
kitminty <electrodkitty@gmail.com>
2026-03-11 10:39:40 -0400

    Added zoom stuff and put references in README.md

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
 If you are on MacOS you need to use the "-XstartOnFirstThread --enable-native-access=ALL-UNNAMED" JVM args to get it to run correctly
+
+Save for later reference:
+
+        glfwGetMouseButton(WindowID, 0) //left click
+        glfwGetMouseButton(WindowID, 1) //right click
+
+        glLineWidth(10.0f); //for GL_LINE_STRIP
+        glPointSize(10.0f); //for GL_POINTS
+
+        glBegin(GL_LINE_STRIP);
+        glColor3f(1.0f, 0.3f, 0.3f);
+        glVertex2f(0.0f, 0.0f);
+        glVertex2f((float)lemx(Time), (float)lemy(Time));
+        glEnd();
+
+        glBegin(GL_POINTS);
+        glColor3f(0.3f, 1.0f, 1.0f);
+        glVertex2f((float)lemx(Time), (float)lemy(Time));
+        glEnd();
+
+        glBegin(GL_POLYGON);
+        glColor3f(0.3f, 1.0f, 1.0f);
+        glVertex2f(0.5f,0.5f);
+        glVertex2f(-0.5f,0.5f);
+        glVertex2f(-0.5f,-0.5f);
+        glVertex2f(0.5f,-0.5f);
+        glEnd();
+
+        glBegin(GL_LINES);
+        glColor3f(0.3f, 1.0f, 1.0f);
+        for(double i=0.0; i<Math.TAU; i += 0.01) {
+            glVertex2f((float)lemx(i),(float)lemy(i));
+            glVertex2f((float)lemx(i+0.01),(float)lemy(i+0.01));
+        }
+        glEnd();
+
+        glBegin(GL_LINES);
+        glColor3f(0.3f, 1.0f, 1.0f);
+        for(double i=0.0; i<Math.TAU; i += 0.01) {
+            glVertex2f((float)(0.8*Math.cos(i)),(float)(0.8*Math.sin(i)));
+            glVertex2f((float)(0.8*Math.cos(i+0.01)),(float)(0.8*Math.sin(i+0.01)));
+        }
+        glEnd();
+
+        glPointSize(10.0f);
+        glBegin(GL_POINTS);
+        glColor3f(1.0f, 0.3f, 0.3f);
+        glVertex2f((float) GetCursorPosX(WindowID), (float) GetCursorPosY(WindowID));
+        glEnd();
--- src/main/java/kitminty/Main.java
+++ src/main/java/kitminty/Main.java
@@ -2,11 +2,13 @@ package kitminty;
 
 
 import org.lwjgl.BufferUtils;
+import org.lwjgl.glfw.GLFWScrollCallback;
 import org.lwjgl.opengl.GL;
 
 import java.awt.*;
 import java.nio.DoubleBuffer;
 import java.nio.IntBuffer;
+import java.util.Arrays;
 
 import static java.sql.Types.NULL;
 import static org.lwjgl.glfw.GLFW.*;
@@ -14,7 +16,7 @@ import static org.lwjgl.opengl.GL11.*;
 
 //Isaac was here
 public class Main {
-     static void main() {
+    void main() {
         glfwInit();
 
 
@@ -38,33 +40,15 @@ public class Main {
         }
     }
 
-    static void renderPolygon(long WindowID) {
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        /*
-        glfwGetMouseButton(WindowID, 0) //left click
-        glfwGetMouseButton(WindowID, 1) //right click
+    void renderPolygon(long WindowID) {
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); //resets the frame every loop
 
-        glLineWidth(10.0f); //for GL_LINE_STRIP
-        glPointSize(10.0f); //for GL_POINTS
-
-        glBegin(GL_LINE_STRIP);
-        glColor3f(1.0f, 0.3f, 0.3f);
-        glVertex2f(0.0f, 0.0f);
-        glVertex2f((float)lemx(Time), (float)lemy(Time));
-        glEnd();
-
-        glBegin(GL_POINTS);
-        glColor3f(0.3f, 1.0f, 1.0f);
-        glVertex2f((float)lemx(Time), (float)lemy(Time));
-        glEnd();
-
-        glBegin(GL_POLYGON);
-        glColor3f(0.3f, 1.0f, 1.0f);
-        glVertex2f(0.5f,0.5f);
-        glVertex2f(-0.5f,0.5f);
-        glVertex2f(-0.5f,-0.5f);
-        glVertex2f(0.5f,-0.5f);
-        glEnd();
+        glfwSetScrollCallback(WindowID, new GLFWScrollCallback() { //finds zoom by finding scroll amount
+            @Override
+            public void invoke(long window, double xoffset, double yoffset) {
+                Zoom = Zoom + yoffset/2;
+            }
+        });
 
         glBegin(GL_LINES);
         glColor3f(0.3f, 1.0f, 1.0f);
@@ -73,53 +57,39 @@ public class Main {
             glVertex2f((float)lemx(i+0.01),(float)lemy(i+0.01));
         }
         glEnd();
-
-        glBegin(GL_LINES);
-        glColor3f(0.3f, 1.0f, 1.0f);
-        for(double i=0.0; i<Math.TAU; i += 0.01) {
-            glVertex2f((float)(0.8*Math.cos(i)),(float)(0.8*Math.sin(i)));
-            glVertex2f((float)(0.8*Math.cos(i+0.01)),(float)(0.8*Math.sin(i+0.01)));
-        }
-        glEnd();
-        */
-
-        glPointSize(10.0f);
-        glBegin(GL_POINTS);
-        glColor3f(1.0f, 0.3f, 0.3f);
-        glVertex2f((float) GetCursorPosX(WindowID), (float) GetCursorPosY(WindowID));
-        glEnd();
-
-        System.out.println(glfwGetMouseButton(WindowID, 0));
+        System.out.println(Zoom);
     }
 
-    public static double GetCursorPosX(long WindowID) {
+    double Zoom = 0;
+
+    public double GetCursorPosX(long WindowID) {
          DoubleBuffer posX = BufferUtils.createDoubleBuffer(1);
          glfwGetCursorPos(WindowID, posX, null);
          return (posX.get()-WindowSizeX(WindowID)/2f)/(WindowSizeX(WindowID)/2f);
     }
-    public static double GetCursorPosY(long WindowID) {
+    public double GetCursorPosY(long WindowID) {
         DoubleBuffer posY = BufferUtils.createDoubleBuffer(1);
         glfwGetCursorPos(WindowID, null, posY);
         return -(posY.get()-WindowSizeY(WindowID)/2f)/(WindowSizeY(WindowID)/2f);
     }
 
-    public static int WindowSizeX(long WindowID) {
+    public int WindowSizeX(long WindowID) {
         IntBuffer posX = BufferUtils.createIntBuffer(1);
         glfwGetWindowSize(WindowID, posX, null);
         return posX.get();
     }
 
-    public static int WindowSizeY(long WindowID) {
+    public int WindowSizeY(long WindowID) {
         IntBuffer posY = BufferUtils.createIntBuffer(1);
         glfwGetWindowSize(WindowID, null, posY);
         return posY.get();
     }
 
-    public static double lemx(double t) {
+    public double lemx(double t) {
         return 0.8*((2*Math.cos(t))/(3-Math.cos(2*t)));
     }
 
-    public static double lemy(double t) {
+    public double lemy(double t) {
         return 0.8*((Math.sin(2*t))/(3-Math.cos(2*t)));
     }
 }
